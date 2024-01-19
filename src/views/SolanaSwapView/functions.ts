665e5b5e538b96471f2a6cd32a33f192d344e35f
Hieu Do <hieudm@yunero.io>
2024-01-19 12:02:34 +0700

    Init

---
+++ src/views/SolanaSwapView/functions.ts
@@ -0,0 +1,267 @@
+import * as anchor from "@project-serum/anchor";
+import * as spl from '@solana/spl-token';
+import { getAssociatedTokenAddressSync, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { env } from "./data";
+const tokenMint = new anchor.web3.PublicKey(env.swap_token);
+import { Keypair, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+
+interface PDAParam {
+  key: anchor.web3.PublicKey,
+  bump: number
+}
+
+const getAtaAccount = async (wallet: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> => {
+
+  let userAssociatedTokenAccount = getAssociatedTokenAddressSync(
+    tokenMint,
+    wallet
+  )
+  return userAssociatedTokenAccount
+}
+
+export const getUserData = async ({ program, wallet }) => {
+  const [user_storage_pda] = await PublicKey.findProgramAddress(
+    [Buffer.from("user_storage"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+  let user_storage_data;
+  try {
+    user_storage_data = await program.account.userStorage.fetch(
+      user_storage_pda
+    );
+  } catch (err) {
+    user_storage_data = null;
+  }
+  return user_storage_data
+}
+
+export const mint = async ({
+  program,
+  wallet,
+  amount,
+  referrer_code
+}) => {
+  const token_x_mint = new PublicKey(
+    "Y5d4m2u3spAE5cgfT3RYbjozTSR4MVTMsAnPiQH7USZ"
+  );
+
+  const [user_storage_pda] = await PublicKey.findProgramAddress(
+    [Buffer.from("user_storage"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+  let user_storage_data;
+  try {
+    console.log(program.account.userStorage)
+    user_storage_data = await program.account.userStorage.fetch(
+      user_storage_pda
+    );
+  } catch (err) {
+    user_storage_data = null;
+  }
+  debugger
+  // random my refcode with 5 char length
+  let my_ref_code = Math.random().toString(36).substring(2, 7);
+  const mint_amount = amount;
+  const mint_type = new anchor.BN(1);
+  if (user_storage_data && user_storage_data.refCode != "") {
+    my_ref_code = user_storage_data.refCode;
+    const [referrer_storage_pda] = await PublicKey.findProgramAddress(
+      [Buffer.from("user_storage"), user_storage_data.refBy.toBuffer()],
+      program.programId
+    );
+    let referrer_storage_data = await program.account.userStorage.fetch(
+      referrer_storage_pda
+    );
+    referrer_code = referrer_storage_data.refCode;
+  }
+
+  const [config_pda] = await PublicKey.findProgramAddress(
+    [Buffer.from("config")],
+    program.programId
+  );
+
+  const [base_referral_ref_code_account_pda] =
+    await PublicKey.findProgramAddress(
+      [Buffer.from("referral"), Buffer.from(referrer_code, "utf8")],
+      program.programId
+    );
+
+  const [vault_x_pda] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from("vault"), token_x_mint.toBuffer()],
+    program.programId
+  );
+
+
+  let user_token_x_ata: any;
+  try {
+    user_token_x_ata = getAssociatedTokenAddressSync(
+      token_x_mint,
+      wallet.publicKey)
+
+  } catch (err) {
+    user_token_x_ata = null;
+  }
+  if (!user_token_x_ata) {
+    alert("balance is 0")
+    return
+  }
+
+  const refCodeAccountData = await program.account.refCode.fetch(
+    base_referral_ref_code_account_pda
+  );
+  const [my_referral_code_account_pda] = await PublicKey.findProgramAddress(
+    [Buffer.from("referral"), Buffer.from(my_ref_code, "utf8")],
+    program.programId
+  );
+
+  const referrer_token_x_ata = getAssociatedTokenAddressSync(
+    token_x_mint,
+    refCodeAccountData.owner
+  );
+  console.log({
+      tokenX: token_x_mint,
+      signer: wallet.publicKey,
+      userStorage: user_storage_pda,
+      vaultX: vault_x_pda,
+      senderTokenX: user_token_x_ata,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      config: config_pda,
+      referralRefCodeAccount: base_referral_ref_code_account_pda,
+      myRefCodeAccount: my_referral_code_account_pda,
+      referrerTokenX: referrer_token_x_ata,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    })
+  await program.rpc.mintNft(mint_amount, mint_type, referrer_code, my_ref_code, {
+    accounts: {
+      tokenX: token_x_mint,
+      signer: wallet.publicKey,
+      userStorage: user_storage_pda,
+      vaultX: vault_x_pda,
+      senderTokenX: user_token_x_ata,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      config: config_pda,
+      referralRefCodeAccount: base_referral_ref_code_account_pda,
+      myRefCodeAccount: my_referral_code_account_pda,
+      referrerTokenX: referrer_token_x_ata,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    },
+    signers: [],
+  });
+};
+
+export const claim = async ({
+  program,
+  wallet,
+}) => {
+  const token_x_mint = new PublicKey(
+    "Y5d4m2u3spAE5cgfT3RYbjozTSR4MVTMsAnPiQH7USZ"
+  );
+
+  const [user_storage_pda] = await PublicKey.findProgramAddress(
+    [Buffer.from("user_storage"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+
+  const user_storage_data = await program.account.userStorage.fetch(
+    user_storage_pda
+  );
+
+  const [config_pda, _] = await PublicKey.findProgramAddress(
+    [Buffer.from("config")],
+    program.programId
+  );
+
+  const [vault_x_pda] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from("vault"), token_x_mint.toBuffer()],
+    program.programId
+  );
+
+  const user_token_x_ata = getAssociatedTokenAddressSync(
+    token_x_mint,
+    wallet.publicKey
+  );
+
+  await program.rpc.claimReward({
+    accounts: {
+      tokenX: token_x_mint,
+      signer: wallet.publicKey,
+      userStorage: user_storage_pda,
+      vaultX: vault_x_pda,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      config: config_pda,
+      receiverTokenX: user_token_x_ata,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    },
+    signers: [],
+  });
+
+};
+
+export const getCurrentInterest = async ({
+  program,
+  wallet,
+}) => {
+  const [config_pda] = await PublicKey.findProgramAddress(
+    [Buffer.from("config")],
+    program.programId
+  );
+  const config_account_data = await program.account.config.fetch(config_pda);
+
+  const nft_bonus_percentages = config_account_data.nftBonusPercentages.map(
+    (el) => Number(el.toString())
+  );
+  const nft_balance_thresholds = config_account_data.nftBalanceThresholds.map(
+    (el) => Number(el.toString())
+  );
+  const nft_speed_earnings = config_account_data.nftSpeedEarnings.map((el) =>
+    Number(el.toString())
+  );
+
+  const [user_storage_pda] = await PublicKey.findProgramAddress(
+    [Buffer.from("user_storage"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+  let user_storage_data: any;
+  try {
+
+    user_storage_data = await program.account.userStorage.fetch(
+      user_storage_pda
+    );
+  } catch (err) {
+
+  }
+  if (!user_storage_data) return 0;
+
+  let pendingClaim = Number(user_storage_data.pendingClaim.toString());
+  const nft_balances = user_storage_data.nftBalances.map((el) =>
+    Number(el.toString())
+  );
+  let duration =
+    Math.floor(Date.now() / 1000) -
+    Number(user_storage_data.lastClaimAt.toString());
+  let total_nft = nft_balances.reduce((acc, el) => acc + el, 0);
+  let bonus_percent = 0;
+  for (let i = nft_balance_thresholds.length - 1; i >= 0; i--) {
+    if (total_nft >= nft_balance_thresholds[i]) {
+      bonus_percent = nft_bonus_percentages[i];
+      break;
+    }
+  }
+  for (let i = 0; i < nft_balances.length; i++) {
+    if (nft_balances[i] > 0) {
+      let second_earn = nft_speed_earnings[i] / (60 * 60);
+      pendingClaim +=
+        (nft_balances[i] * duration * second_earn * (10000 + bonus_percent)) /
+        10000;
+    }
+  }
+
+  return pendingClaim / LAMPORTS_PER_SOL;
+}
---
+++ src/views/SolanaSwapView/index.tsx
@@ -0,0 +1,216 @@
+import Link from "next/link";
+import { FC, useEffect, useState } from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import { HomeIcon, UserIcon } from "@heroicons/react/outline";
+import orderBy from "lodash.orderby";
+
+import { Loader, SelectAndConnectWalletButton } from "components";
+import * as anchor from "@project-serum/anchor";
+
+import { SolanaLogo } from "components";
+import styles from "./index.module.css";
+import { mint, getCurrentInterest, claim, getUserData } from "./functions";
+import { useProgram } from "./useProgram";
+
+const endpoint = "https://explorer-api.devnet.solana.com";
+
+const connection = new anchor.web3.Connection(endpoint);
+
+export const SolanaSwapView: FC = ({ }) => {
+  const [isAirDropped, setIsAirDropped] = useState(false);
+
+  const wallet = useAnchorWallet();
+
+  return (
+    <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
+      <div className={styles.container}>
+        <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+          <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+              <span className="text-4xl">🌔</span>
+            </button>
+          </div>
+          <div className="flex-1 px-2 mx-2">
+            <div className="text-sm breadcrumbs">
+              <ul className="text-xl">
+              </ul>
+            </div>
+          </div>
+
+          <div className="flex-none">
+            <WalletMultiButton className="btn btn-ghost" />
+          </div>
+        </div>
+
+        <div className="text-center pt-2">
+          <div className="hero min-h-16 pt-4">
+            <div className="text-center hero-content">
+              <div className="max-w-lg">
+                <h1 className="mb-5 text-5xl">
+                  MINT NFT DEMO <SolanaLogo />
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div className="flex justify-center">
+          {!wallet ? (
+            <SelectAndConnectWalletButton onUseWalletClick={() => { }} />
+          ) : (
+            <SwapScreen />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const SwapScreen = () => {
+  const wallet: any = useAnchorWallet();
+  const [swaps, setSwaps] = useState<unknown[]>([]);
+  const { program } = useProgram({ connection, wallet });
+  const [lastUpdatedTime, setLastUpdatedTime] = useState<number>();
+  const [currentInterest, setCurrentInterest] = useState(0);
+  const [userData, setUserData] = useState<any>();
+  useEffect(() => {
+  }, [wallet, lastUpdatedTime]);
+
+
+  useEffect(() => {
+    if (!wallet) return;
+    //@ts-ignore
+    const intervalId = setInterval(async () => {
+      if (program && wallet) {
+        const interest = await getCurrentInterest({ program, wallet });
+        setCurrentInterest(interest);
+        const userData = await getUserData({ program, wallet });
+        setUserData(userData);
+      }
+    }, 1 * 1000)
+    return function cleanup() {
+      clearInterval(intervalId);
+    };
+
+  }, [wallet]);
+
+
+  const onSwapSent = (swapEvent: unknown) => {
+    setSwaps((prevState) => ({
+      ...prevState,
+      swapEvent,
+    }));
+  };
+
+  return (
+    <div className="rounded-lg flex justify-center">
+
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-xs">
+          <p>
+            {`Current Interest: ` + currentInterest}
+          </p>
+          <p>
+            {JSON.stringify(userData)}
+          </p>
+        </div>
+        <div className="text-xs">
+          <NetSwap onSwapSent={onSwapSent} />
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+type NetSwap = {
+  onSwapSent: (t: any) => void;
+};
+
+const NetSwap: FC<NetSwap> = ({ onSwapSent }) => {
+  const wallet: any = useAnchorWallet();
+  const { program } = useProgram({ connection, wallet });
+  const [content, setContent] = useState<string>("");
+  const [value, setValue] = useState<any>(5)
+  const [referrer_code, setReferrerCode] = useState<any>("ABCDE")
+  const onContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+    if (value) {
+      setContent(value);
+    }
+  };
+
+  const onMintClick = async () => {
+    if (!program) return;
+
+    const amount = new anchor.BN(Number(value));
+
+    const mint_result = await mint({
+      program,
+      wallet,
+      amount,
+      referrer_code
+    });
+
+
+    console.log("New swap transaction succeeded: ", mint_result);
+    setContent("");
+    onSwapSent(mint_result);
+  };
+
+  const onClaimClick = async () => {
+    if (!program) return;
+
+    const claim_result = await claim({
+      program,
+      wallet
+    });
+  };
+
+  console.log(value)
+  function isNumeric(value: any) {
+    return /^[0-9]{0,9}(\.[0-9]{1,2})?$/.test(value);
+  }
+
+  return (
+    <div style={{ minWidth: 240 }} className="mb-8 pb-4 border-b border-gray-500 flex ">
+
+      <div className="w-full">
+        <input value={value} onChange={(e) => {
+          const value = e.target.value
+          console.log(value)
+          setValue(value)
+        }
+        } placeholder="Enter the Referrer Code" className="mb-4"></input>
+        <input value={referrer_code} onChange={(e) => {
+          const value = e.target.value
+          setReferrerCode(value)
+        }
+      } 
+      type="text"
+        placeholder="Enter referrer code" className="mb-4"></input>
+
+        <button
+          className="btn btn-primary rounded-full normal-case	w-full"
+          onClick={onMintClick}
+          style={{ minHeight: 0, height: 40 }}
+        >
+          Mint
+        </button>
+        <br />
+        <br />
+        <button
+          className="btn btn-primary rounded-full normal-case	w-full"
+          onClick={onClaimClick}
+          style={{ minHeight: 0, height: 40 }}
+        >
+          Claim
+        </button>
+      </div>
+    </div>
+  );
+};
